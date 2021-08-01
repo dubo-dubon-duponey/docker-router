88142c0a42453c6998a1e34c25bc75b1fa3dfa70
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-07-31 21:21:59 -0700

    Move to 2
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- context/runtime/boot/entrypoint.sh
+++ context/runtime/boot/entrypoint.sh
@@ -1,25 +1,60 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 
-# Ensure the certs folder is writable
-[ -w "/certs" ] || {
-  >&2 printf "/certs is not writable. Check your mount permissions.\n"
+[ -w /certs ] || {
+  printf >&2 "/certs is not writable. Check your mount permissions.\n"
   exit 1
 }
 
-[ -w "/tmp" ] || {
-  >&2 printf "/tmp is not writable. Check your mount permissions.\n"
+[ -w /tmp ] || {
+  printf >&2 "/tmp is not writable. Check your mount permissions.\n"
   exit 1
 }
 
-mkdir -p /tmp/cache
+[ -w /data ] || {
+  printf >&2 "/data is not writable. Check your mount permissions.\n"
+  exit 1
+}
 
-# Specifics to this image
-HTTPS_PORT="${HTTPS_PORT:-}"
-STAGING="${STAGING:-}"
+# Helpers
+case "${1:-run}" in
+  # Short hand helper to generate password hash
+  "hash")
+    shift
+    printf >&2 "Generating password hash\n"
+    caddy hash-password -algorithm bcrypt "$@"
+    exit
+  ;;
+  # Helper to get the ca.crt out (once initialized)
+  "cert")
+    if [ "$TLS" == "" ]; then
+      printf >&2 "Your container is not configured for TLS termination - there is no local CA in that case."
+      exit 1
+    fi
+    if [ "$TLS" != "internal" ]; then
+      printf >&2 "Your container uses letsencrypt - there is no local CA in that case."
+      exit 1
+    fi
+    if [ ! -e /certs/pki/authorities/local/root.crt ]; then
+      printf >&2 "No root certificate installed or generated. Run the container so that a cert is generated, or provide one at runtime."
+      exit 1
+    fi
+    cat /certs/pki/authorities/local/root.crt
+    exit
+  ;;
+  "run")
+    # Bonjour the container if asked to. While the PORT is no guaranteed to be mapped on the host in bridge, this does not matter since mDNS will not work at all in bridge mode.
+    if [ "${MDNS_ENABLED:-}" == true ]; then
+      goello-server -name "$MDNS_NAME" -host "$MDNS_HOST" -port "$PORT" -type "$MDNS_TYPE" &
+    fi
 
-args=(caddy -conf /config/caddy.conf -agree -disable-http-challenge -https-port "$HTTPS_PORT" -http-port 1080)
+    # If we want TLS and authentication, start caddy in the background
+    if [ "$TLS" ]; then
+      exec HOME=/tmp/caddy-home exec caddy run -config /config/caddy/main.conf --adapter caddyfile
+    fi
+  ;;
+esac
 
-[ ! "$STAGING" ] || args+=(-ca https://acme-staging-v02.api.letsencrypt.org/directory)
-
-exec "${args[@]}" "$@"
+# args=(caddy -conf /config/caddy.conf -agree -disable-http-challenge -https-port "$HTTPS_PORT" -http-port 1080)
+# [ ! "$STAGING" ] || args+=(-ca https://acme-staging-v02.api.letsencrypt.org/directory)
+# exec "${args[@]}" "$@"
