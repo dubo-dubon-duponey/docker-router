5ac52254dc1867d055e5fef5695ac69446ae2c43
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-09-02 14:09:07 -0700

    Fixing compilation / cruft
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- context/build/main.go
+++ context/build/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 
-	_ "github.com/miekg/caddy-prometheus"
+	_ "github.com/caddyserver/caddy/v2/modules/standard"
+
+	// _ "github.com/miekg/caddy-prometheus"
 	_ "github.com/sillygod/cdp-cache"
-	_ "github.com/caddyserver/forwardproxy"
-	_ "github.com/dhaavi/caddy-permission"
+	// _ "github.com/caddyserver/forwardproxy"
+	// _ "github.com/dhaavi/caddy-permission"
 	_ "github.com/caddyserver/replace-response"
 )
 
 func main() {
-	caddycmd.EnableTelemetry = false
-	caddycmd.Run()
+	caddycmd.Main()
 }
